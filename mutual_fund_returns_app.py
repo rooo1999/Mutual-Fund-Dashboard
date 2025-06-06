25c4fa253cf4cac78bd8ec471330be93c7bb3d56
rooo1999 <rohangoyal1999exam@gmail.com>
2025-06-06 16:52:36 +0530

    Initial commit

--- mutual_fund_returns_app.py
+++ mutual_fund_returns_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
